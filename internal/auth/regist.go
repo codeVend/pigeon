4d0a742789ecb1760ed9a593745ca0ea3f6e96e8
flaschblood <dlubavin20@gmail.com>
2026-03-24 19:22:58 +0300

    Update regist.go

--- internal/auth/regist.go
+++ internal/auth/regist.go
@@ -7,18 +7,18 @@ import (
 func CheckCookies() {
 	
 }
+type Service struct{}
 
+func (s *Service) Register(user *User); error {
+	if user.Username == "" {
+		return fmt.Errorf("Имя пустое")
+	} else {
+		fmt.Printf("Пользователь %s успешно зарегистрирован", user.Username)
+		return nil
+	}
+}
 func NewUser(Username string, Number string, Password string) {
 
-	type Service struct{}
-
-	func (s *Service) Register(user *User); error {
-		if user.Username == "" {
-			return fmt.Errorf("Имя пустое")
-		} else {
-			fmt.Printf("Пользователь %s успешно зарегистрирован", user.Username)
-			return nil
-		}
-	}
+	
 
 }
