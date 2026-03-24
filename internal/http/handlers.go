f7a9d439244b983b233a5ec06358a1f264a3a918
flaschblood <dlubavin20@gmail.com>
2026-03-24 19:24:23 +0300

    Update handlers.go

--- internal/http/handlers.go
+++ internal/http/handlers.go
@@ -4,17 +4,17 @@ import (
 		"github.com/codeVend/pigeon/internal/auth"
 		"net/http"
 	)
+type Handler struct {
+	AuthService *auth.Service
+}
 
+func (h *Handler) HandleRegister(u *auth.User) error {
+	return h.AuthService.Register(u)
+}
 func RegisterHandler(w http.ResponseWriter, r *http.Request) {
 
 	
 
-	type Handler struct {
-		AuthService *auth.Service
-	}
-
-	func (h *Handler) HandleRegister(u *auth.User); error {
-		return h.AuthService.Register(u)
-	}
+	
 }
 
